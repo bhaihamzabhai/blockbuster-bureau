c7ab42e29f314981025d22ef2a5b52831ca88911
bhaihamzabhai <hamzaali5236@gmail.com>
2026-09-05 21:25:12 +0500

    update csp for youtube oembed and apis

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://www.googletagservices.com; connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com; frame-src 'self' https://www.youtube.com; img-src 'self' data: https:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://www.googletagservices.com; connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com https://*.youtube.com; frame-src 'self' https://www.youtube.com https://*.youtube.com; img-src 'self' data: https:;",
           },
         ],
       },
